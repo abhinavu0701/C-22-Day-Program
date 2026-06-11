c0c262273354b29e43ddd6becc4f3c41cde3cbd5
abhinavu0701 <abhinav.07ai@gmail.com>
2026-06-11 16:56:06 +0530

    Day07 Labs

---
+++ Day07/Labs/ASP.NET Core Web App/ASP.NET Core Web App/Views/Appointment/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.NET_Core_Web_App.Views.Appointment
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Day07/Labs/ASP.NET Core Web App/ASP.NET Core Web App/Views/Doctor/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.NET_Core_Web_App.Views.Doctor
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
